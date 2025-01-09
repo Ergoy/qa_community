1c43cce47ebfb3cd88568e50c6b0646915715496
Ergoy <1959507363@qq.com>
2025-01-09 10:13:04 +0800

    添加表单组件

--- src/main.js
+++ src/main.js
@@ -9,7 +9,15 @@ import usePopper from "./controllers/usePopper"
 import dispatchTo from "./controllers/dispatch_to"
 import breakpoints from "./controllers/breakpoints"
 import tooltip from "./controllers/tooltip";
+import FullscreenController from "./controllers/fullscreen_controller.js"
+import MoreController from "./controllers/more_controller"
+import StickyController from "./controllers/sticky_controller"
+import UploadController from "./controllers/upload_controller.js"
+import tomSelect from "./controllers/tomSelect";
+import SmartForegroundColorController from "./controllers/smart_foreground_color_controller"
+import ColorPickerController from "./controllers/color_picker_controller"
 
+Alpine.directive("tom-select", tomSelect);
 Alpine.directive("tooltip", tooltip);
 Alpine.store("breakpoints", breakpoints)
 window.Alpine = Alpine
@@ -25,6 +33,10 @@ window.Stimulus = application
 application.register('theme', ThemeController)
 application.register('dropdown', Dropdown)
 application.register('load_more', LoadMoreController)
-
-
+application.register('fullscreen', FullscreenController)
+application.register("more", MoreController)
+application.register("sticky", StickyController)
+application.register("upload", UploadController)
+application.register("smart-foreground-color", SmartForegroundColorController)
+application.register("color-picker", ColorPickerController)
 
